ba89c3435800477383ed18c657a60c291e8bccb1
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-04-11 21:38:38 +0200

    add training script [run-train]

---
+++ train.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+acc = accuracy_score(y_test, pred)
+
+print("Training completed")
+print("Accuracy:", acc)
+
+with open("model_info.txt","w") as f:
+    f.write("run-id")
