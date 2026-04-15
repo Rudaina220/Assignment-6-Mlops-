b3bb6adb907274be3d42a7783a2002c861498185
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-04-15 13:23:02 +0200

    Update train1.py

--- train1.py
+++ train1.py
@@ -3,22 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+raise Exception("Intentional failure for pipeline test")
+
 data = load_iris()
 X = data.data
 y = data.target
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-pred = model.predict(X_test)
-acc = accuracy_score(y_test, pred)
-
-print("Training completed")
-print("Accuracy:", acc)
-
-with open("model_info.txt", "w") as f1:
-    f1.write("run-id")
