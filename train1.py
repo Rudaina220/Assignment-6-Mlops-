5ce25813e81e17df8c9e1abf8858ae81c92fa8be
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-04-15 13:28:40 +0200

    test training pipeline [run-train]

--- train1.py
+++ train1.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_iris
 
-raise Exception("Intentional failure for pipeline test")
+raise Exception("Intentional failure for pipeline test 1")
 
 data = load_iris()
 X = data.data
