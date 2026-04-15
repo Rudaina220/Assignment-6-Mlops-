f864c999b4a0bc71e8a2c1af2ae61bb5095539bb
Rudaina220 <s-rudaina.salmin@zewailcity.edu.eg>
2026-04-15 13:24:02 +0200

    Update train1.py

--- train1.py
+++ train1.py
@@ -1,7 +1,4 @@
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 raise Exception("Intentional failure for pipeline test")
 
